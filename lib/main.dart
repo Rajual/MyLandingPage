194c5accfcfd911ac6036e1becec8ee78ab43876
Julio Rodriguez <julioalfonso8003@gmail.com>
2021-09-13 17:00:29 -0500

    Created the float menu

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'My landing page',
       initialRoute: '/home',
       onGenerateRoute: Flurorouter.router.generator,
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:my_landing_page/ui/views/home_view.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      color: Colors.red,
+    return Scaffold(
+      body: Stack(
+        children: [
+          const _HomeBody(),
+          Positioned(
+              right: 20,
+              top: 20,
+              child: Container(
+                width: 150,
+                height: 50,
+                color: Colors.black,
+              ))
+        ],
+      ),
+    );
+  }
+}
+
+class _HomeBody extends StatelessWidget {
+  const _HomeBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: [
+        const HomeView(),
+      ],
     );
   }
 }
