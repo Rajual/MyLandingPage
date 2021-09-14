4251b753200a32f914eda0f2b3d1669d8fc75d61
Rajual <julioalfonso8003@gmail.com>
2021-09-14 06:25:10 -0500

    Created 'about_view.dart'

--- lib/ui/views/about_view.dart
+++ lib/ui/views/about_view.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({Key? key}) : super(key: key);
+class AboutView extends StatelessWidget {
+  const AboutView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.pink,
+      color: Colors.green,
       child: Center(
         child: FittedBox(
           child: Text(
-            'Home',
+            'About',
             style: GoogleFonts.montserratAlternates(
                 fontSize: 80, fontWeight: FontWeight.bold),
           ),
