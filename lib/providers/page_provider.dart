b14fa0d93c2c240bbddf49916d31efcadb4af7d0
Rajual <julioalfonso8003@gmail.com>
2021-09-15 06:34:45 -0500

    Added change to the url on the windows

--- lib/providers/page_provider.dart
+++ lib/providers/page_provider.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart' as html;
+
+//import 'dart:html' as html;
 
 class PageProvider extends ChangeNotifier {
   PageController scrollController = PageController();
+
+  final List<String> _pages = ['home', 'about', 'contact'];
+
   goTo(int index) {
+    final routeName = _pages[index];
+
+    html.window.history.pushState(null, 'none', '#/$routeName');
+
     scrollController.animateToPage(index,
         duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
   }
