03edbca655f2a3326cbbe85d157eebf4d7d41cb9
Julio Rodriguez <julioalfonso8003@gmail.com>
2021-09-15 10:57:08 -0500

    Changes in the pages name to changer the url

--- lib/providers/page_provider.dart
+++ lib/providers/page_provider.dart
@@ -7,19 +7,27 @@ class PageProvider extends ChangeNotifier {
   PageController scrollController = PageController();
 
   final List<String> _pages = ['home', 'about', 'contact'];
-//TODO: Fixe it.
-  createScrollController(String routerName){
-    this.scrollController=new PageController(initialPage: )
+
+  int _currentIndex = 0;
+  createScrollController(String routeName) {
+    scrollController = PageController(initialPage: getPageIndex(routeName));
+    html.document.title = _pages[getPageIndex(routeName)];
+    scrollController.addListener(() {
+      final int index = (scrollController.page ?? 0).round();
+      if (index != _currentIndex) {
+        html.window.history.pushState(null, 'none', '#/${_pages[index]}');
+        _currentIndex = index;
+        html.document.title = _pages[index];
+      }
+    });
   }
 
-  int getPageIndex(String routrName){
-    return (!_pages.contains(routrName))?0:_pages.indexOf(routrName);
+  int getPageIndex(String routrName) {
+    return (!_pages.contains(routrName)) ? 0 : _pages.indexOf(routrName);
   }
 
   goTo(int index) {
-    final routeName = _pages[index];
-
-    html.window.history.pushState(null, 'none', '#/$routeName');
+    html.window.history.pushState(null, 'none', '#/${_pages[index]}');
 
     scrollController.animateToPage(index,
         duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
--- lib/routers/router_handlers.dart
+++ lib/routers/router_handlers.dart
@@ -1,10 +1,15 @@
 import 'package:fluro/fluro.dart';
+import 'package:my_landing_page/providers/page_provider.dart';
 import 'package:my_landing_page/ui/pages/home_page.dart';
+import 'package:provider/provider.dart';
 
 final homeHandler = Handler(handlerFunc: (context, params) {
   final page = params['page']!.first;
 
   if (page != '/') {
+    final PageProvider pageProvider =
+        Provider.of<PageProvider>(context!, listen: false);
+    pageProvider.createScrollController(page);
     return const HomePage();
   }
 });
